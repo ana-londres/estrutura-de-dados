a1c2c8678a36bafde52d17826344b0d966263111
ana-londres <ana.londres@academico.ufpb.br>
2024-07-24 21:36:57 -0300

    Update program

--- ordenacao/script.sh
+++ ordenacao/script.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
-# Compila o programa
+# Executar o algoritmo em um arquivo específico
+executar_algoritmo() {
+    algoritmo=$1
+    arquivo=$2
+    echo "Testando $algoritmo sort com o arquivo de entrada $arquivo"
+    ./main $algoritmo $arquivo
+    echo
+}
+
 g++ -o main main.cpp selection-sort.cpp insertion-sort.cpp
 
-# Limpa os arquivos de log
 > selection_sort_log.txt
 > insertion_sort_log.txt
-> comparison_log.txt
+> comparacao_log.txt
 
-# Executa os testes
-for algorithm in selection insertion; do
-    for file in instancias-num/*.in; do
-        echo "Testando $algorithm sort com o arquivo de entrada $file"
-        ./main $algorithm $file
-        echo
+if [ "$#" -eq 2 ]; then
+    # Se apenas um arquivo específico for fornecido, executa o algoritmo
+    algoritmo=$1
+    arquivo=$2
+    executar_algoritmo $algoritmo $arquivo
+else
+    # Executa os testes para todos os arquivos e algoritmos
+    for algoritmo in selection insertion; do
+        for arquivo in instancias-num/*.in; do
+            executar_algoritmo $algoritmo $arquivo
+        done
     done
-done
 
-# Cria um arquivo de comparação e compara os tempos de execução
-echo "Tempos de execução (selection-sort vs insertion-sort)" > comparison_log.txt
+    # Cria um arquivo de comparação e compara os tempos de execução
+    echo "Tempos de execução (selection-sort vs insertion-sort)" > comparacao_log.txt
 
-for file in instancias-num/*.in; do
-    selection_time=$(grep "$file" selection_sort_log.txt | awk -F '; ' '{print $3}' | awk '{print $4}')
-    insertion_time=$(grep "$file" insertion_sort_log.txt | awk -F '; ' '{print $3}' | awk '{print $4}')
-    echo "Instancia: $file; Selection: $selection_time segundos; Insertion: $insertion_time segundos" >> comparison_log.txt
-done
+    for arquivo in instancias-num/*.in; do
+        selection_time=$(grep "$arquivo" selection_sort_log.txt | awk -F '; ' '{print $3}' | awk '{print $4}')
+        insertion_time=$(grep "$arquivo" insertion_sort_log.txt | awk -F '; ' '{print $3}' | awk '{print $4}')
+        echo "Instancia: $arquivo; Selection: $selection_time segundos; Insertion: $insertion_time segundos" >> comparacao_log.txt
+    done
 
-echo "Comparação dos tempos de execução completa."
+    echo "Comparação dos tempos de execução completa."
+fi
